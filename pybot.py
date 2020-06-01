e5cda9b7244937199efb9ca5aa7e191a3a9d80f9
taka5963 <big-hill-filial-piety-333-@docomo.ne.jp>
2020-06-01 10:54:07 +0900

    initial_commit

--- pybot.py
+++ pybot.py
@@ -10,12 +10,15 @@ def len_command(command):
 
 def heisei_command(command):
     heisei, year_str = command.split()
-    year = int(year_str)
-    if year >= 1989:
-        heisei_year = year - 1988
-        response = '西暦{}年ハ、平成{}年デス'.format(year, heisei_year)
-    else:
-        response = '西暦{}年ハ、平成デハアリマセン'.format(year)
+    try:
+        year = int(year_str)
+        if year >= 1989:
+            heisei_year = year - 1988
+            response = '西暦{}年ハ、平成{}年デス'.format(year, heisei_year)
+        else:
+            response = '西暦{}年ハ、平成デハアリマセン'.format(year)
+    except ValueError:
+        response = '数値ヲ入力シテクダサイ'
     return response
 
 command_file = open('pybot.txt', encoding = 'utf-8')
