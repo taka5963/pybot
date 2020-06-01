0888eaf621a639d0272c61a817ffce99830887db
taka5963 <big-hill-filial-piety-333-@docomo.ne.jp>
2020-06-01 11:22:14 +0900

    例外の出力

--- pybot.py
+++ pybot.py
@@ -36,31 +36,36 @@ for line in lines:
 while True:
     command = input('pybot> ')
     response = ''
-    for key in bot_dict:
-        if key in command:
-            response = bot_dict[key]
+    try:
+        for key in bot_dict:
+            if key in command:
+                response = bot_dict[key]
+                break
+
+        if '平成' in command:
+            response = heisei_command(command)
+        if '長さ' in command:
+            response = len_command(command)
+        if '干支' in command:
+            response = eto_command(command)
+        if '選ぶ' in command:
+            response = choice_command(command)
+        if 'さいころ' in command:
+            response = dice_command()
+        if '今日' in command:
+            response = today_command()
+        if '現在' in command:
+            response = now_command()
+        if '曜日' in command:
+            response = weekday_command(command)
+
+        if not response:
+            response = '何ヲ言ッテルカ、ワカラナイ'
+        print(response)
+
+        if 'さようなら' in command:
             break
-
-    if '平成' in command:
-        response = heisei_command(command)
-    if '長さ' in command:
-        response = len_command(command)
-    if '干支' in command:
-        response = eto_command(command)
-    if '選ぶ' in command:
-        response = choice_command(command)
-    if 'さいころ' in command:
-        response = dice_command()
-    if '今日' in command:
-        response = today_command()
-    if '現在' in command:
-        response = now_command()
-    if '曜日' in command:
-        response = weekday_command(command)
-
-    if not response:
-        response = '何ヲ言ッテルカ、ワカラナイ'
-    print(response)
-
-    if 'さようなら' in command:
-        break
+    except Exception as e:
+        print('予期セヌ エラーガ 発生シマシタ')
+        print('* 種類', type(e))
+        print('* 内容', e)
